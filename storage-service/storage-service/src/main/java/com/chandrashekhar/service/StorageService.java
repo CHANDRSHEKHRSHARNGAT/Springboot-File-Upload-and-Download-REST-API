9280dafa2f9ae2bd2e28e01e518d1e217969ee81
Chandrashekhar sharnagat <47698158+CHANDRSHEKHRSHARNGAT@users.noreply.github.com>
2022-10-09 14:54:44 +0530

    File Upload and Download REST API
     Pringboot File Upload and Download REST API

---
+++ storage-service/storage-service/src/main/java/com/chandrashekhar/service/StorageService.java
@@ -0,0 +1,36 @@
+package com.chandrashekhar.service;
+
+import com.chandrashekhar.entity.ImageData;
+import com.chandrashekhar.repository.StorageRepository;
+import com.chandrashekhar.util.ImageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class StorageService {
+
+    @Autowired
+    private StorageRepository repository;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+
+        ImageData imageData = repository.save(ImageData.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .imageData(ImageUtils.compressImage(file.getBytes())).build());
+        if (imageData != null) {
+            return "file uploaded successfully : " + file.getOriginalFilename();
+        }
+        return null;
+    }
+
+    public byte[] downloadImage(String fileName){
+        Optional<ImageData> dbImageData = repository.findByName(fileName);
+        byte[] images=ImageUtils.decompressImage(dbImageData.get().getImageData());
+        return images;
+    }
+}
